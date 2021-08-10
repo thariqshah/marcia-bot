b8196054e940d0241b2d957053c0d4743fe93382
thariq <thariq.m@perfomatix.com>
2021-08-10 12:28:09 +0530

    initial commit

---
+++ src/main/java/com/bot/marcia/MarciaApplication.java
@@ -0,0 +1,13 @@
+package com.bot.marcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/marcia/MarciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.marcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
